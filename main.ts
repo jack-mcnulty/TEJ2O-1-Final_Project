f8c7c7b96454fb6728052397c272f85044d2baa7
jack-mcnulty <jack.mcnulty@stu.ocsb.ca>
2026-01-08 13:28:52 -0500

    added the code for the servo

--- main.ts
+++ main.ts
@@ -2,6 +2,14 @@
  *
  * Created by: Jack
  * Created on: Jan 2026
- * This program does something
+ * This program makes a game with a stepper motor and a servo
 */
 
+
+
+input.onButtonPressed(Button.A, function() {
+    servos.P0.setAngle(90)
+    basic.pause(1500)
+    servos.P0.setAngle(0)
+})
+            
